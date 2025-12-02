dd8b6b364bb4cccaed66703a56f3c7b06d58fff9
patelmanan112 <patelmanan112@gmail.com>
2025-12-02 13:33:17 +0530

    Print sum of 2nd row in 2d matrix

---
+++ que1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+
+    int mat[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int sum = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        sum += mat[1][i];
+    }
+    printf("%d", sum);
+    return 0;
+}
